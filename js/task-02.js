f7a0f7a753e8a60f0ed74ab24a91bf4b4b12a24a
volodymyrt1978 <volodymyr.telyovych@gmail.com>
2022-07-24 18:05:26 +0300

    tasks 2 & 3

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,34 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const list = document.querySelector("#ingredients");
+
+const firstEl = document.createElement("li");
+firstEl.textContent = ingredients[0];
+firstEl.classList.add("item");
+
+const secondEl = document.createElement("li");
+secondEl.textContent = ingredients[1];
+secondEl.classList.add("item");
+
+const thirdEl = document.createElement("li");
+thirdEl.textContent = ingredients[2];
+thirdEl.classList.add("item");
+
+const forthEl = document.createElement("li");
+forthEl.textContent = ingredients[3];
+forthEl.classList.add("item");
+
+const fifthEl = document.createElement("li");
+fifthEl.textContent = ingredients[4];
+fifthEl.classList.add("item");
+
+const sixthEl = document.createElement("li");
+sixthEl.textContent = ingredients[5];
+sixthEl.classList.add("item");
+
+
+list.after(firstEl, secondEl, thirdEl, forthEl, fifthEl, sixthEl);
+
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,14 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const list = document.querySelector(".gallery")
+
+const markup = images
+  .map((image) =>
+    `<li><img src="${image.url}" alt="${image.alt}"></li>`)
+  .join("");
+  
+// console.log(markup);
+
+list.insertAdjacentHTML("beforeend", markup);
