a9f0b88d7b042a858dd3772ecade7a1ec5d304fc
volodymyrt1978 <volodymyr.telyovych@gmail.com>
2022-07-24 23:38:11 +0300

    task-01 completed

--- js/task-01.js
+++ js/task-01.js
@@ -1,26 +1,14 @@
-const categoryItems = document.querySelectorAll(".item");
-console.log("Number of categories: ", categoryItems.length);
+const arrayOfItems = document.querySelectorAll(".item ul");
+console.log("Number of categories: ", arrayOfItems.length);
 
-const categories = document.querySelector("#categories");
+arrayOfItems.forEach((item) => {
+    const titleOfItem = item.previousElementSibling.textContent;
+    const element = item.children;
+    const amountOfElements = element.length;
+    console.log(`Category: ${titleOfItem}`);
+    console.log(`Elements: ${amountOfElements}`);
+})
 
-const firstBlock = categories.firstElementChild;
-const secondBlock = firstBlock.nextElementSibling;
-const thirdBlock = categories.lastElementChild;
 
-
-const titleOfFirstBlock = firstBlock.firstElementChild.textContent;
-const numberOfFirstBlock = firstBlock.lastElementChild.children.length;
-console.log("Category: ", titleOfFirstBlock);
-console.log("Elements: ", numberOfFirstBlock);
-
-const titleOfSecondBlock = secondBlock.firstElementChild.textContent;
-const numberOfSecondBlock = secondBlock.lastElementChild.children.length;
-console.log("Category: ", titleOfSecondBlock);
-console.log("Elements: ", numberOfSecondBlock);
-
-const titleOfThirdBlock = thirdBlock.firstElementChild.textContent;
-const numberOfThirdBlock = thirdBlock.lastElementChild.children.length;
-console.log("Category: ", titleOfThirdBlock);
-console.log("Elements: ", numberOfThirdBlock);
 
 
