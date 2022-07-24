5f60a0bfe1615f915e2d8191e9a9c4e32af63584
volodymyrt1978 <volodymyr.telyovych@gmail.com>
2022-07-24 13:42:31 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+const arrayOfCategories = document.querySelectorAll(".item");
+console.log("Number of categories: ", arrayOfCategories.length);
+
+const categoryTitle = document.querySelector("h2");
+console.log(categoryTitle.textContent);
