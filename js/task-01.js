5de8804e7e48779d7df769743b77a226c46721de
volodymyrt1978 <volodymyr.telyovych@gmail.com>
2022-07-24 15:49:24 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,24 +3,24 @@ console.log("Number of categories: ", categoryItems.length);
 
 const categories = document.querySelector("#categories");
 
-const blockOfAnimals = categories.firstElementChild;
-const blockOfProducts = blockOfAnimals.nextElementSibling;
-const blockOfTechnologies = categories.lastElementChild;
+const firstBlock = categories.firstElementChild;
+const secondBlock = firstBlock.nextElementSibling;
+const thirdBlock = categories.lastElementChild;
 
 
-const titleOfAnimals = blockOfAnimals.firstElementChild.textContent;
-const numberOfAnimals = blockOfAnimals.lastElementChild.children.length;
-console.log("Category: ", titleOfAnimals);
-console.log("Elements: ", numberOfAnimals);
+const titleOfFirstBlock = firstBlock.firstElementChild.textContent;
+const numberOfFirstBlock = firstBlock.lastElementChild.children.length;
+console.log("Category: ", titleOfFirstBlock);
+console.log("Elements: ", numberOfFirstBlock);
 
-const titleOfProducts = blockOfProducts.firstElementChild.textContent;
-const numberOfProducts = blockOfProducts.lastElementChild.children.length;
-console.log("Category: ", titleOfProducts);
-console.log("Elements: ", numberOfProducts);
+const titleOfSeccondBlock = secondBlock.firstElementChild.textContent;
+const numberOfSeccondBlock = secondBlock.lastElementChild.children.length;
+console.log("Category: ", titleOfSeccondBlock);
+console.log("Elements: ", numberOfSeccondBlock);
 
-const titleOfTechnologies = blockOfTechnologies.firstElementChild.textContent;
-const numberOfTechnologies = blockOfTechnologies.lastElementChild.children.length;
-console.log("Category: ", titleOfTechnologies);
-console.log("Elements: ", numberOfTechnologies);
+const titleOfThirdBlock = thirdBlock.firstElementChild.textContent;
+const numberOfThirdBlock = thirdBlock.lastElementChild.children.length;
+console.log("Category: ", titleOfThirdBlock);
+console.log("Elements: ", numberOfThirdBlock);
 
 
