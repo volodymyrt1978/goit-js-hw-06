34796ab2ea135b7e0a42adeef9aa4e35e6563d5a
volodymyrt1978 <volodymyr.telyovych@gmail.com>
2022-07-24 15:38:24 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,26 @@
-const arrayOfCategories = document.querySelectorAll(".item");
-console.log("Number of categories: ", arrayOfCategories.length);
+const categoryItems = document.querySelectorAll(".item");
+console.log("Number of categories: ", categoryItems.length);
 
-const categoryTitle = document.querySelector("h2");
-console.log(categoryTitle.textContent);
+const categories = document.querySelector("#categories");
+
+const blockOfAnimals = categories.firstElementChild;
+const blockOfProducts = blockOfAnimals.nextElementSibling;
+const blockOfTechnologies = categories.lastElementChild;
+
+
+const titleOfAnimals = blockOfAnimals.firstElementChild.textContent;
+const numberOfAnimals = blockOfAnimals.lastElementChild.children.length;
+console.log("Category: ", titleOfAnimals);
+console.log("Elements: ", numberOfAnimals);
+
+const titleOfProducts = blockOfProducts.firstElementChild.textContent;
+const numberOfProducts = blockOfProducts.lastElementChild.children.length;
+console.log("Category: ", titleOfProducts);
+console.log("Elements: ", numberOfProducts);
+
+const titleOfTechnologies = blockOfTechnologies.firstElementChild.textContent;
+const numberOfTechnologies = blockOfTechnologies.lastElementChild.children.length;
+console.log("Category: ", titleOfTechnologies);
+console.log("Elements: ", numberOfTechnologies);
+
+
