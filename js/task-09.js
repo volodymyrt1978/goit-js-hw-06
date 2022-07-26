eada812445909465786b3b9c1233e23e1a48bc0c
volodymyrt1978 <volodymyr.telyovych@gmail.com>
2022-07-26 15:33:25 +0300

    task-09 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+const changeColorButton = document.querySelector('.change-color');
+console.log(changeColorButton);
+
+const valueOfColor = document.querySelector('.color');
+console.log(valueOfColor.textContent);
+
+changeColorButton.addEventListener("click", handleClickOnChangeColorButton);
+
+function handleClickOnChangeColorButton(event) {
+  const currentColor = getRandomHexColor()
+  document.body.style.backgroundColor = currentColor;
+  valueOfColor.textContent = currentColor;
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
