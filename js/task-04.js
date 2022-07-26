2d186622c3b490ca7c4d582f89ca47cd5bf0c5ab
volodymyrt1978 <volodymyr.telyovych@gmail.com>
2022-07-26 15:41:32 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -3,23 +3,24 @@ let counterValue = 0;
 const decrementButton = document.querySelector('button[data-action="decrement"]');
 const incrementButton = document.querySelector('button[data-action="increment"]');
 
+const decrementValue = decrementButton.textContent;
+const incrementValue = incrementButton.textContent;
 
 decrementButton.addEventListener('click', handleDecrementButtonClick);
-
 
 incrementButton.addEventListener('click', handleIncrementButtonClick);
 
 function handleDecrementButtonClick() {
-    updateDisplay(counterValue -= 1);
+    updateDisplay(counterValue += Number(decrementValue));
     
 }
 
 function handleIncrementButtonClick() {
-    updateDisplay(counterValue += 1);
+    updateDisplay(counterValue += Number(incrementValue));
     
 }
 
 function updateDisplay(value) {
-    document.querySelector('#value').innerHTML = value;
+    document.querySelector('#value').textContent = value;
 }
 
