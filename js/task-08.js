2a7e0c887fea1f24e0d175434b41a04f057e6347
volodymyrt1978 <volodymyr.telyovych@gmail.com>
2022-07-26 13:57:43 +0300

    task-08 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    
+    event.preventDefault();
+    
+    const {
+        elements: { email, password } 
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        window.alert('ALL THE FIELDS SHOULD BE FILLED IN');
+    } 
+    else {
+        console.log(`Email: ${email.value}, Password: ${password.value}`)
+    }
+
+    event.currentTarget.reset();
+}
